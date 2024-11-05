1c142b3e6a62ce9c67a191e38824df4827f5ae67
mashcatg <mashcatg@gmail.com>
2024-11-05 16:27:33 +0600

    1

--- send_message.php
+++ send_message.php
@@ -1,6 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Credentials: true");
 session_start();
 
 include 'db.php'; // Include database connection
