c05696d438d433f4c7616de82b93bcddff742a5c
mashcatg <120169459+mashcatg@users.noreply.github.com>
2024-11-05 17:08:43 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 // Connect to the database
-$host = 'localhost';
+$host = '127.0.0.1';
 $dbname = 'lms';
 $username = 'root'; 
-$password = '';
-$port = '3306';
+$password = 'mashPass789!@#';
+// $port = '3306';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
